3770375194950d421e56813129843b9d729f9200
yurec0098 <yurec0098@rambler.ru>
2021-05-09 10:10:37 +0300

    Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.

--- Lesson_5/Program.cs
+++ Lesson_5/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace Lesson_5
 {
@@ -6,7 +8,13 @@ namespace Lesson_5
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Веедите текст для сохранения в файл text_1.txt");
+
+			string line = Console.ReadLine();
+			File.WriteAllText("text_1.txt", line, Encoding.UTF8);
+
+			Console.WriteLine($"В файл text_1.txt записан текст '{line}'");
+			Console.ReadLine();
 		}
 	}
 }
