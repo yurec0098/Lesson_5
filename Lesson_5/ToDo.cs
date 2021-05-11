01f628c0f8ebf6a11466af3b612a91e3a9e9ebca
yurec0098 <yurec0098@rambler.ru>
2021-05-11 07:55:34 +0300

    upd file name
    upd
    
     Список задач (ToDo-list):
    написать приложение для ввода списка задач;
    задачу описать классом ToDo с полями Title и IsDone;
    на старте, если есть файл tasks.json/xml/bin (выбрать формат), загрузить из него массив имеющихся задач и вывести их на экран;
    если задача выполнена, вывести перед её названием строку «[x]»;
    вывести порядковый номер для каждой задачи;
    при вводе пользователем порядкового номера задачи отметить задачу с этим порядковым номером как выполненную;
    записать актуальный массив задач в файл tasks.json/xml/bin.

---
+++ Lesson_5/ToDo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson_5
+{
+	[Serializable]
+	public class ToDo
+	{
+		public override string ToString() => $"{(IsDone ? "[x] " : "")}{Title}";
+
+		public string Title { get; set; }
+		public bool IsDone { get; set; }
+
+		public ToDo() { }
+		public ToDo(string title)
+		{
+			Title = title;
+		}
+	}
+}
