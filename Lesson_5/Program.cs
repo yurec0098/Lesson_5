78752b48214080fdadb798756207e577e76ff2e6
yurec0098 <yurec0098@rambler.ru>
2021-05-13 16:17:43 +0300

    upd

--- Lesson_5/Program.cs
+++ Lesson_5/Program.cs
@@ -7,6 +7,7 @@ namespace Lesson_5
 	{
 		static void Main(string[] args)
 		{
+			//	записываем только время, о дате в условии ни слова
 			File.AppendAllText("startup.txt", $"{DateTime.Now:T}\n");
 			Console.WriteLine("При запуске в файл startup.txt было записано текущее время");
 
